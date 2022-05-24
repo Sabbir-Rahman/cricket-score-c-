be60e67265d5f9b01f3605f8777d26adb297ef6d
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-05-24 15:50:20 +0600

    command class function added

--- Command.cs
+++ Command.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace CricketScore
 {
-    internal class Command
+    class Command
     {
+        public void dotBall()
+        {
+
+        }
+
+        public void single()
+        {
+
+        }
+
+        public void twoRun()
+        {
+
+        }
+
+        public void threeRun()
+        {
+
+        }
+
+        public void fourRun()
+        {
+
+        }
+
+        public void fiveRun()
+        {
+
+        }
+
+        public void sixRun()
+        {
+
+        }
+
+        public void wicket()
+        {
+
+        }
+
+        public void wide()
+        {
+
+        }
+
+        public void noBall()
+        {
+
+        }
+
+        
     }
 }
