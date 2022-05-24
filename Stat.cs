b4132e129439ab9b7d593b50052cd29a0150129d
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-05-24 16:01:48 +0600

    stat class updated

---
+++ Stat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketScore
+{
+    internal class Stat
+    {
+    }
+}
